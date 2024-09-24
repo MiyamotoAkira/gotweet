1fef82c6310de79f2538b0880d85a8c04d05fb1f
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2024-09-24 20:55:52 +0100

    Post test with authorization working

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -1,11 +1,13 @@
 package main_test
 
 import (
-	sut "github.com/MiyamotoAkira/gotweet/routes"
-	"github.com/stretchr/testify/assert"
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	sut "github.com/MiyamotoAkira/gotweet/routes"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBasicFlow(t *testing.T) {
@@ -18,4 +20,21 @@ func TestBasicFlow(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestBasicPost(t *testing.T) {
+	router := sut.SetupRouter()
+
+	w := httptest.NewRecorder()
+
+	var jsonStr = []byte(`{"value":"weee"}`)
+	req, _ := http.NewRequest("POST", "/admin", bytes.NewBuffer(jsonStr))
+
+	req.Header.Add("authorization", "Basic Zm9vOmJhcg==")
+	req.Header.Add("content-type", "application/json")
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "{\"status\":\"ok\"}", w.Body.String())
 }
